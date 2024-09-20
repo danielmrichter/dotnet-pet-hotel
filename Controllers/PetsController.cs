13f228ea910be26cca4cf985f045e885a2de9bdf
Emma Molden <emma.molden007@gmail.com>
2024-09-20 10:43:26 -0500

    removed comment from petsController

--- Controllers/PetsController.cs
+++ Controllers/PetsController.cs
@@ -20,8 +20,6 @@ namespace pet_hotel.Controllers
             _context = context;
         }
 
-        // This is just a stub for GET / to prevent any weird frontend errors that 
-        // occur when the route is missing in this controller
         [HttpGet]
         public IEnumerable<Pet> pets()
         {
